152586c796c0f992a489f61e4bdc60f7a56f55da
Nguyen Anh Bao <baotrass@gmail.com>
2023-06-03 15:35:12 +0700

    Change Event to a wrapper of std::variant<...> Move Event out of Window namespace

---
+++ GameFrame/Window/Event.cpp
@@ -0,0 +1,12 @@
+#include "Event.hpp"
+
+namespace HJUIK
+{
+    namespace Window
+    {
+        Event::Data& Event::getData()
+        {
+            return mData;
+        }
+    } // namespace Window
+} // namespace HJUIK
--- GameFrame/Window/Event.hpp
+++ GameFrame/Window/Event.hpp
@@ -8,53 +8,79 @@
 #include "Keyboard.hpp"
 #include "Mouse.hpp"
 
+#include <variant>
+
 namespace HJUIK
 {
-	namespace Window
+	namespace EventType
 	{
-		struct Event
+		// Event struct lists
+		// #1 Window changed size
+		struct Resize
 		{
-			struct Size
-			{
-				unsigned int Width;
-				unsigned int Height;
-			};
-			struct MousePosition
-			{
-				int X;
-				int Y;
-			};
-			union
-			{
-				// Size of the screen after resizing
-				Size Resize;
-				// Position of mouse
-				MousePosition MousePos;
-				// Delta of mouse wheel
-				float MouseDelta;
-				// Mouse button 
-				Mouse::Button Button;
-				// Keyboard key pressed
-				Keyboard::Key Key;
-			} Data;
-
-			// Event type
-			enum Type
-			{
-				// Window
-				RESIZED = 0, CLOSED, LOST_FOCUS, GAINED_FOCUS,
-				// Keyboard
-				KEY_PRESSED, KEY_RELEASED,
-				// Mouse
-				MOUSE_BUTTON_PRESSED, MOUSE_BUTTON_RELEASED,
-				MOUSE_SCROLLED, MOUSE_MOVED,
-
-				TYPE_COUNT,
-				// Not specified (Other irrelevant event)
-				OTHER
-			} Type;
+			unsigned int Width;
+			unsigned int Height;
 		};
-	} // namespace Window
+		// #2 Window closed
+		struct Close { };
+		// #3 Window gained focus
+		struct GainFocus { };
+		// #4 Window lost focus
+		struct LoseFocus { };
+		// #5 Keyboard pressed
+		struct KeyPress
+		{
+			// Actually scancode, but we only care about it so "Code" for short
+			Window::Keyboard::Key Code;
+		};
+		// #6 Keyboard released
+		struct KeyRelease
+		{
+			Window::Keyboard::Key Code;
+		};
+		// #7 Mouse button pressed
+		struct MousePress
+		{
+			Window::Mouse::Button Button;
+		};
+		// #8 Mouse button released
+		struct MouseRelease
+		{
+			Window::Mouse::Button Button;
+		};
+		// #9 Mouse wheel scrolled
+		struct MouseScroll
+		{
+			// The offset of the mouse wheel. Positive value means scrolling up.
+			float Delta;
+		};
+		// #10 Mouse moved
+		struct MouseMove
+		{
+			int X;
+			int Y;
+		};
+	} // namespace EventType
+
+	// Represent system events happening during runtime
+	class Event
+	{
+	public:
+		using Data = std::variant<
+			EventType::Resize,
+			EventType::Close,
+			EventType::GainFocus,
+			EventType::LoseFocus,
+			EventType::KeyPress,
+			EventType::KeyRelease,
+			EventType::MousePress,
+			EventType::MouseRelease,
+			EventType::MouseScroll,
+			EventType::MouseMove>;
+		Data& getData();
+	private:
+		Data mData;
+	};
 } // namespace HJUIK
 
 #endif
