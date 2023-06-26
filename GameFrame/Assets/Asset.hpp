6b04012d9ac037df9f83698371f11ef4d96bf2b6
ngoduyanh <ngoduyanh.chip@gmail.com>
2023-06-26 15:49:38 +0700

    Fix indenting

--- GameFrame/Assets/Asset.hpp
+++ GameFrame/Assets/Asset.hpp
@@ -13,13 +13,13 @@ namespace HJUIK
 	{
 		class Asset;
 
-        // returned from `registerUpdateHandler`
-        // destroy this to unregister the handler
+		// returned from `registerUpdateHandler`
+		// destroy this to unregister the handler
 		class UpdateHandle
 		{
 		public:
-            // default constructor will initialize this handle
-            // to an invalid state (`mAsset` is nullptr, `id` is 0)
+			// default constructor will initialize this handle
+			// to an invalid state (`mAsset` is nullptr, `id` is 0)
 			UpdateHandle();
 			UpdateHandle(std::weak_ptr<Asset> asset, std::size_t id);
 			~UpdateHandle();
@@ -36,29 +36,29 @@ namespace HJUIK
 			std::size_t mID;
 		};
 
-        // Asset should always be stored in a shared_ptr in order to share
-        // common assets throughout the program.
+		// Asset should always be stored in a shared_ptr in order to share
+		// common assets throughout the program.
 		class Asset : public std::enable_shared_from_this<Asset>
 		{
 		public:
 			Asset()			 = default;
 			virtual ~Asset() = default;
 
-            // since this lives in a shared_ptr, there's no need for Asset
-            // to be movable/copyable
+			// since this lives in a shared_ptr, there's no need for Asset
+			// to be movable/copyable
 			Asset(const Asset&)					   = delete;
 			Asset(Asset&&)						   = delete;
 			auto operator=(const Asset&) -> Asset& = delete;
 			auto operator=(Asset&&) -> Asset&	   = delete;
 
-            // register an update handler
-            // NOTE: keep the return value to keep this register alive
+			// register an update handler
+			// NOTE: keep the return value to keep this register alive
 			auto registerUpdateHandler(std::function<void()> callback) -> UpdateHandle;
 
 		protected:
-            // call this every time the asset update
-            // e.g. file content change, asset dependencies updated, etc.
-            // this will dispatch all active update handlers
+			// call this every time the asset update
+			// e.g. file content change, asset dependencies updated, etc.
+			// this will dispatch all active update handlers
 			auto assetUpdated() -> void;
 
 		private:
