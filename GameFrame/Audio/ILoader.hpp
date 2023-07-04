5110d50b0e2018f1a4eb64ccb376cc634ff2c7ab
ngoduyanh <ngoduyanh.chip@gmail.com>
2023-07-04 19:44:44 +0700

    Add seeking API to ILoader
    (please reformat the files before committing)

--- GameFrame/Audio/ILoader.hpp
+++ GameFrame/Audio/ILoader.hpp
@@ -2,7 +2,7 @@
 #define GAMEFRAME_AUDIO_ILOADER_HPP
 
 /*
-    Base class for sound loader
+	Base class for sound loader
 */
 
 #include <iostream>
@@ -10,26 +10,30 @@
 
 namespace HJUIK
 {
-    namespace Audio
-    {   
-        class SoundBuffer;
-        class ILoader
-        {
-        public:
-            ILoader() = default;
-            virtual ~ILoader() = default;
-            ILoader(const ILoader&) = delete;
-            ILoader(ILoader&&) = delete;
-            auto operator = (const ILoader&) -> ILoader& = delete;
-            auto operator = (ILoader&&) -> ILoader& = delete;
+	namespace Audio
+	{
+		class SoundBuffer;
+		class ILoader
+		{
+		public:
+			ILoader()								   = default;
+			virtual ~ILoader()						   = default;
+			ILoader(const ILoader&)					   = delete;
+			ILoader(ILoader&&)						   = delete;
+			auto operator=(const ILoader&) -> ILoader& = delete;
+			auto operator=(ILoader&&) -> ILoader&	   = delete;
 
-            // Copy the loaded data into `target`
-            // Do not actually buffer data to OpenAL (this is done inside SoundBuffer)
-            // The loader needs not to be in a valid state for next `buffer` call until next `load` operation
-            // Return true if buffering succeeded
-            virtual auto buffer(SoundBuffer&/*target*/) -> bool = 0;
-        };
-    } // namespace Audio
+			// Copy the loaded data into `target`
+			// Do not actually buffer data to OpenAL (this is done inside SoundBuffer)
+			// The loader needs not to be in a valid state for next `buffer` call until next `load` operation
+			// Return true if buffering succeeded
+			virtual auto buffer(SoundBuffer& /*target*/) -> bool = 0;
+
+			virtual auto channels() -> size_t = 0;
+			virtual auto sampleRate() -> size_t = 0;
+			virtual auto seek(size_t sampleIndex) -> void = 0;
+		};
+	} // namespace Audio
 } // namespace HJUIK
 
 #endif
